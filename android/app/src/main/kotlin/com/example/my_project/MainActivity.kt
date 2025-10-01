797c930e35281d711cc6a14f480455c12b6d4081
gvrob <gareth.roberts@hilltopdigitallab.com>
2025-10-01 11:40:05 +0100

    sahha flutter activity message in MainActivity.kt

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.mycompany.kikonnect
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-import android.os.Bundle
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 3b50fc7 (sahha flutter activity message in MainActivity.kt)
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
