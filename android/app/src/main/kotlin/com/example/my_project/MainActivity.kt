94a8cab2ee570da541b6fb348f21ad3d8df4f67b
gvrob <gareth.roberts@hilltopdigitallab.com>
2025-11-12 14:02:38 +0000

    Local edits: MainActivity.kt + pubspec.yaml

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.mycompany.kikonnect
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.util.Log
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("KiKonnect", "MainActivity extends FlutterFragmentActivity ✅")
+    }
 }
