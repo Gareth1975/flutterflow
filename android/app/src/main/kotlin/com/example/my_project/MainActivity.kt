897db706accbf16694a53af5c6fc205e758204e6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-12 14:55:53 +0000

    Updating to latest FlutterFlow output.

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.mycompany.kikonnect
 
-import android.os.Bundle
-import android.util.Log
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d("KiKonnect", "MainActivity extends FlutterFragmentActivity ✅")
-    }
+class MainActivity: FlutterActivity() {
 }
