047b5375100cdd7ad5605d75fa63ff8e290def9e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-14 18:43:47 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/your_activity/your_activity_widget.dart
+++ lib/pages/your_activity/your_activity_widget.dart
@@ -752,6 +752,26 @@ class _YourActivityWidgetState extends State<YourActivityWidget>
                                   .fontStyle,
                             ),
                       ),
+                      Text(
+                        'Hello World',
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              font: GoogleFonts.inter(
+                                fontWeight: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .fontWeight,
+                                fontStyle: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .fontStyle,
+                              ),
+                              letterSpacing: 0.0,
+                              fontWeight: FlutterFlowTheme.of(context)
+                                  .bodyMedium
+                                  .fontWeight,
+                              fontStyle: FlutterFlowTheme.of(context)
+                                  .bodyMedium
+                                  .fontStyle,
+                            ),
+                      ),
                     ],
                   ),
                 ),
